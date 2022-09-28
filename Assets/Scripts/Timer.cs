b5c5fdc4794ae1ddb6e8b0722a4cfdac5a066c8d
Jim Lougheed <jims.codes@gmail.com>
2022-09-27 22:10:34 -0400

    Describe timer functionality

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,6 +7,8 @@ public class Timer : MonoBehaviour
     [SerializeField] float timeToComplete = 30f;
     [SerializeField] float timeToShowAnswer = 10f;
     public bool questionActive = false;
+    public float fillFraction;
+    public bool loadNextQuestion;
     float timerValue;
 
     // Update is called once per frame
@@ -15,16 +17,29 @@ public class Timer : MonoBehaviour
         UpdateTimer();
     }
 
+    public void CancelTimer() {
+        timerValue = 0;
+    }
+
     void UpdateTimer() {
         timerValue -= Time.deltaTime;
-        if (timerValue <= 0) {
+        if (timerValue > 0) {
+            fillFraction = timerValue / timeToComplete;
+            if (questionActive) {
+                fillFraction = timerValue / timeToComplete;
+            } else {
+                fillFraction = timerValue / timeToShowAnswer;
+            }
+
+        } else {
             if (questionActive) {
                 timerValue = timeToShowAnswer;
             } else {
                 timerValue = timeToComplete;
+                loadNextQuestion = true;
             }
             questionActive = !questionActive;
         }
-        Debug.Log(timerValue);
+        Debug.Log(questionActive + ":" + timerValue + "=" + fillFraction);
     }
 }
